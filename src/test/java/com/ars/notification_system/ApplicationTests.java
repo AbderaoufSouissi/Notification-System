4b150b77fb8961bc412d8af42dcfda285edcaae8
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-05-31 12:55:21 +0100

    add initial configuration

--- src/test/java/com/ars/notification_system/ApplicationTests.java
+++ src/test/java/com/ars/notification_system/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class NotificationSystemApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {

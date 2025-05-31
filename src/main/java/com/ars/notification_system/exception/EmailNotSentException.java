4fae424b3045017448f6f624f279dd9a49c6db30
abderaouf souissi <abderaouf.souissi@outlook.com>
2025-05-31 14:05:46 +0100

    add exception handling

--- src/main/java/com/ars/notification_system/exception/EmailNotSentException.java
+++ src/main/java/com/ars/notification_system/exception/EmailNotSentException.java
@@ -1,5 +1,9 @@
 package com.ars.notification_system.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 public class EmailNotSentException extends RuntimeException {
 
     public EmailNotSentException(String message) {
